23c30cdab9e747b6f14800a62c2f73833ce7435d
Jakub Gorski <jakubg93@o2.pl>
2015-04-27 10:27:38 +0200

    vhvbnv

--- src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 public class RadarTest {
 	@Test
+	//cos do testow
 	public void launchPatriotOnceWhenNoticesAScudMissle() {
 		PatriotBattery batteryMock = mock(PatriotBattery.class);
 		Radar radar = new Radar(batteryMock);
